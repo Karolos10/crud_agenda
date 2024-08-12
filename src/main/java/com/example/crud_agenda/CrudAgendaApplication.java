0971e2f1cbf68dd643ed083410f97e61e1490cce
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-12 16:55:21 -0500

    List of persons in memory has been added

--- src/main/java/com/example/crud_agenda/CrudAgendaApplication.java
+++ src/main/java/com/example/crud_agenda/CrudAgendaApplication.java
@@ -1,13 +1,36 @@
 package com.example.crud_agenda;
 
+import com.example.crud_agenda.entity.Contact;
+import com.example.crud_agenda.repository.ContactRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CrudAgendaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudAgendaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(ContactRepository contactRepository){
+		return args -> {
+			List<Contact> contacts = Arrays.asList(
+					new Contact("Mariela Rodriguez", "mariela@gmail.com", LocalDateTime.now()),
+					new Contact("Karlos Rodriguez", "karlos@gmail.com", LocalDateTime.now()),
+					new Contact("Francisco Lopez", "francisco@gmail.com", LocalDateTime.now()),
+					new Contact("Ana Ferreira", "ana@gmail.com", LocalDateTime.now()),
+					new Contact("Juan Nuñez", "juan@gmail.com", LocalDateTime.now())
+			);
+
+			contactRepository.saveAll(contacts);
+		};
 	}
 
 }
--- src/main/java/com/example/crud_agenda/entity/Contact.java
+++ src/main/java/com/example/crud_agenda/entity/Contact.java
@@ -4,9 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,12 +12,54 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class Contact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NonNull
     private String name;
+    @NonNull
     private String email;
+    @NonNull
     private LocalDateTime createdAt;
+
+    public Contact(String name, String email, LocalDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
